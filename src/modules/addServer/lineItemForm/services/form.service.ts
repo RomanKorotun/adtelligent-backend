402cbfcb1099abf1649544772604255ebd1c47ee
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-10-07 17:07:01 +0300

    fix: correct path to form.html

--- src/modules/addServer/lineItemForm/services/form.service.ts
+++ src/modules/addServer/lineItemForm/services/form.service.ts
@@ -2,7 +2,12 @@ import fs from "fs/promises";
 import path from "path";
 
 export const readFormHtml = async (): Promise<string> => {
-  const htmlPath = path.join(__dirname, "../pages/form.html");
+  const basePath =
+    process.env.NODE_ENV === "production"
+      ? path.join(process.cwd(), "build/modules/addServer/lineItemForm/pages")
+      : path.join(__dirname, "../pages");
+
+  const htmlPath = path.join(basePath, "form.html");
   const html = await fs.readFile(htmlPath, "utf-8");
   return html;
 };
